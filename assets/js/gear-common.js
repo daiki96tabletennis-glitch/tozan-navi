1b5c72e515b1556749c601557380eae5f6ad3352
daiki96tabletennis-glitch <daiki96tabletennis@gmail.com>
2026-09-21 10:44:48 +0900

    改善

--- assets/js/gear-common.js
+++ assets/js/gear-common.js
@@ -10,18 +10,26 @@
 //
 // 取得前や取得失敗時も表示が壊れないよう、これまで山ページで使われていた
 // 値をデフォルトとして保持しておく（fetch前でも同じ結果になる）。
+//
+// Amazon優先順位: 商品別amazonUrl → 商品別amazonAsin → 商品名から検索URL生成
+// 楽天優先順位  : 商品別rakutenUrl → 商品名から検索URL生成
+// （個別指定が無い商品はこれまでと全く同じ検索URLになる）
 
 window.YMGear = (function () {
   var DEFAULT_CONFIG = {
     amazonTag: "amazonafdaiki-22",
     rakutenId: "57384242.53497bb8.57384243.366083eb"
   };
-  var DEFAULT_DIRECT_ASIN = {
-    "メレル MOAB 3 ミッド GTX": "B09BP11RNG"
+  var DEFAULT_PRODUCTS = {
+    "メレル MOAB 3 ミッド GTX": {
+      amazonAsin: "B09BP11RNG",
+      amazonUrl: "",
+      rakutenUrl: ""
+    }
   };
 
   var config = DEFAULT_CONFIG;
-  var directAsin = DEFAULT_DIRECT_ASIN;
+  var products = DEFAULT_PRODUCTS;
   var loadPromise = null;
 
   function normalizeProductName(name) {
@@ -42,8 +50,8 @@ window.YMGear = (function () {
         if (data && data.config) {
           config = Object.assign({}, DEFAULT_CONFIG, data.config);
         }
-        if (data && data.directAsin) {
-          directAsin = Object.assign({}, DEFAULT_DIRECT_ASIN, data.directAsin);
+        if (data && data.products) {
+          products = Object.assign({}, DEFAULT_PRODUCTS, data.products);
         }
       })
       .catch(function () {
@@ -54,14 +62,22 @@ window.YMGear = (function () {
 
   function amazonUrl(productName) {
     var clean = normalizeProductName(productName);
-    if (directAsin[clean]) {
-      return "https://www.amazon.co.jp/dp/" + directAsin[clean] + "?tag=" + config.amazonTag;
+    var p = products[clean];
+    if (p && p.amazonUrl) {
+      return p.amazonUrl;
+    }
+    if (p && p.amazonAsin) {
+      return "https://www.amazon.co.jp/dp/" + p.amazonAsin + "?tag=" + config.amazonTag;
     }
     return "https://www.amazon.co.jp/s?k=" + encodeURIComponent(clean) + "&tag=" + config.amazonTag;
   }
 
   function rakutenUrl(productName) {
     var clean = normalizeProductName(productName);
+    var p = products[clean];
+    if (p && p.rakutenUrl) {
+      return p.rakutenUrl;
+    }
     var searchUrl = "https://search.rakuten.co.jp/search/mall/" + encodeURIComponent(clean) + "/";
     var pcValue = encodeURIComponent(searchUrl);
     return "https://hb.afl.rakuten.co.jp/hgc/" + config.rakutenId + "/?pc=" + pcValue + "&m=" + pcValue;
